d0d8452073e4c7cb25f08aa0340e9434fd9195d6
Phukit-Time <phukit.s@ku.th>
2024-03-27 23:09:04 +0700

    Finish kinkaw app

---
+++ src/main/java/ku/kinkao/repository/MemberRepository.java
@@ -0,0 +1,12 @@
+package ku.kinkao.repository;
+
+
+import ku.kinkao.entity.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.UUID;
+
+@Repository
+public interface MemberRepository extends JpaRepository<Member,UUID> {
+    Member findByUsername(String username);
+}
